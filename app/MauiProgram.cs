32a9eefe3f41e82ffb9b30387640ac6db2e18d00
kuadalma <adammarzec2808@gmail.com>
2023-11-19 20:19:36 +0100

    basic look and viewmodel mvvm

--- app/MauiProgram.cs
+++ app/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace app;
+﻿using app.ViewModel;
+
+namespace app;
 
 public static class MauiProgram
 {
@@ -12,6 +14,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
 
 		return builder.Build();
 	}
---
+++ app/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace app.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        private string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if(string.IsNullOrEmpty(Text))
+                return;
+            Items.Add(Text);
+            Text = string.Empty;
+        }
+    }
+}
