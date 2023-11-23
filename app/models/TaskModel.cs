42f72645d3759bc60a33003ea9ddfddf2daae183
kuadalma <adammarzec2808@gmail.com>
2023-11-23 17:28:44 +0100

    add TaskModel

---
+++ app/models/TaskModel.cs
@@ -0,0 +1,24 @@
+﻿namespace app.models
+{
+    public class TaskModel
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime SetDate { get; set; }
+
+        public TaskModel(string name, DateTime setDate, string desc = "")
+        {
+            Name = name;
+            Desc = desc;
+            CreateDate = DateTime.Today;
+            SetDate = setDate;
+        }
+        public void Edit(string name, DateTime setDate, string desc = "")
+        {
+            Name = name;
+            Desc = desc;
+            SetDate = setDate;
+        }
+    }
+}
