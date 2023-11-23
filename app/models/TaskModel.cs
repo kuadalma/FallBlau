cbcb20efdb563f1d191a8ea7eb9992ffb70f5d4f
kuadalma <adammarzec2808@gmail.com>
2023-11-23 19:55:18 +0100

    change date format

--- app/models/TaskModel.cs
+++ app/models/TaskModel.cs
@@ -4,17 +4,17 @@
     {
         public string Name { get; set; }
         public string Desc { get; set; }
-        public DateTime CreateDate { get; set; }
-        public DateTime SetDate { get; set; }
+        public string CreateDate { get; set; }
+        public string SetDate { get; set; }
 
-        public TaskModel(string name, DateTime setDate, string desc = "")
+        public TaskModel(string name, string setDate, string desc = "")
         {
             Name = name;
             Desc = desc;
-            CreateDate = DateTime.Today;
+            CreateDate = DateTime.Today.ToString("dd/MM/yyyy");
             SetDate = setDate;
         }
-        public void Edit(string name, DateTime setDate, string desc = "")
+        public void Edit(string name, string setDate, string desc = "")
         {
             Name = name;
             Desc = desc;
